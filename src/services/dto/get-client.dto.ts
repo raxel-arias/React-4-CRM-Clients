d06eb8a2bb05e206d47d64421aae1ec0fcd8c165
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-30 17:59:22 -0500

    Set default value to ClientGot interface

--- src/services/dto/get-client.dto.ts
+++ src/services/dto/get-client.dto.ts
@@ -3,3 +3,12 @@ import Client from "./client.entity";
 export interface ClientGot extends Client {
     _id: string;
 }
+
+export const DefaultClientGot: ClientGot = {
+    _id: '',
+    name: '',
+    company: '',
+    notes: '',
+    email: '',
+    phone: ''
+}
