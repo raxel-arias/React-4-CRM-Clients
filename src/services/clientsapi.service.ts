f0cbea44c43d9bf3288c66fc5feb43bd4d573f14
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-30 14:14:21 -0500

    Modify error response

--- src/services/clientsapi.service.ts
+++ src/services/clientsapi.service.ts
@@ -4,42 +4,39 @@ import {CreateClientDto} from "./dto/create-client.dto";
 export default class ClientsApiService {
     private API_URL = 'http://localhost:3000/api/v1/clients';
 
-    public async getClients() {
-        try {
-            const {data, status} = await axios.get(this.API_URL);
+    public getClients() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const {data, status} = await axios.get(this.API_URL);
 
-            return {
-                data,
-                status
+                resolve( {
+                    data,
+                    status
+                });
+            } catch (error: any) {
+                reject({
+                    error
+                });
             }
-        } catch (error: any) {
-            return this.handleException(error);
-        }
+        })
     }
 
-    public async createClient(createClientDto: CreateClientDto) {
-        try {
-            const { data, status } =  await axios.post(this.API_URL, createClientDto, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
+    public createClient(createClientDto: CreateClientDto) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const { data, status } =  await axios.post(this.API_URL, createClientDto, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
 
-            return {
-                data,
-                status
-            };
-        } catch (error: any) {
-            return this.handleException(error);
-        }
-
-    }
-
-    private handleException(error: any): any {
-        if (error.response) {
-            return error.response.data;
-        }
-
-        return error;
+                resolve({
+                   data,
+                   status
+                });
+            } catch (error: any) {
+                reject(error);
+            }
+        });
     }
 }
