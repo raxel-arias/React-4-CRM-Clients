f51f79957fececf37ca75eb3e23428819df123ae
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-31 17:23:38 -0500

    Update logic in Form.tsx Component to let edit clients

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,18 +1,35 @@
 import { useNavigate } from "react-router-dom";
+
 import {Formik, Form as FormikForm, Field, FormikHelpers} from 'formik';
-
-import {IClient, DefaultNewClientObj} from "../../interfaces/client.interface";
 import {NewClientSchema} from "../../validations";
-import ValidationError from "./ValidationError";
-import ClientsApiService from "../../services/clientsapi.service";
 
-const Form = (): JSX.Element => {
+import ClientsApiService from "../../services/clientsapi.service";
+import {ClientGot} from "../../services/dto/get-client.dto";
+import {UpdateClientDto} from "../../services/dto/update-client.dto";
+
+import ValidationError from "./ValidationError";
+
+import {FormCP} from "../../interfaces/form/form-component-props.interface";
+import {IClient, DefaultNewClientObj} from "../../interfaces/client.interface";
+
+const Form = (props: FormCP): JSX.Element => {
     const clientsApiService = new ClientsApiService();
     const navigator = useNavigate();
 
-    async function handleSubmit(data: IClient, {resetForm}: FormikHelpers<IClient>) {
+    const {
+        editing,
+        clientToEdit
+    } = props;
+
+    async function handleSubmit(data: IClient, {resetForm}: FormikHelpers<IClient>): Promise<void> {
+        if (editing) return await updateClientInfo(data as ClientGot, resetForm);
+
+        await createClient(data, resetForm);
+    }
+
+    async function createClient(newData: IClient, resetForm: any): Promise<void> {
         try {
-            const response = await clientsApiService.createClient(data);
+            const response = await clientsApiService.createClient(newData);
 
             resetForm();
             navigator('/clients');
@@ -22,11 +39,36 @@ const Form = (): JSX.Element => {
         }
     }
 
+    async function updateClientInfo(updatedData: ClientGot, resetForm: any): Promise<void> {
+        try {
+            const response = await clientsApiService.updateClient(clientToEdit?._id!, sanitizeDataToUpdate(updatedData));
+
+            resetForm();
+            navigator('/clients');
+        } catch (error: any) {
+            alert('Hubo un error');
+            console.error(error);
+        }
+    }
+
+    function sanitizeDataToUpdate(updatedData: ClientGot): UpdateClientDto {
+        const {_id, createdAt, updatedAt, __v, ...filteredData} = updatedData
+        const clientSanitizedData: UpdateClientDto = {...filteredData};
+
+        return clientSanitizedData;
+    }
+
+    function getFormDefaultValues(): IClient {
+        return editing && clientToEdit? clientToEdit as IClient : DefaultNewClientObj;
+    }
+
     return (
         <div className={"bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto"}>
-            <h2 className={"text-gray-600 font-bold text-xl uppercase text-center"}>Add Client</h2>
 
-            <Formik initialValues={DefaultNewClientObj} onSubmit={handleSubmit} validationSchema={NewClientSchema}>
+            <Formik
+                initialValues={getFormDefaultValues()}
+                onSubmit={handleSubmit}
+                validationSchema={NewClientSchema}>
                 {({errors, touched}) => {
                     return(
                         <FormikForm className="mt-10">
@@ -84,7 +126,7 @@ const Form = (): JSX.Element => {
                             <div className="mb-4">
                                 <label
                                     htmlFor="phone"
-                                    className="text-gray-800">Phone
+                                    className="text-gray-800">Phone <span className="text-gray-500">(Optional)</span>
                                 </label>
                                 <Field
                                     id="phone"
@@ -100,7 +142,7 @@ const Form = (): JSX.Element => {
                             <div className="mb-4">
                                 <label
                                     htmlFor="notes"
-                                    className="text-gray-800">Notes
+                                    className="text-gray-800">Notes <span className="text-gray-500">(Optional)</span>
                                 </label>
                                 <Field as="textarea"
                                     id="notes"
@@ -115,7 +157,7 @@ const Form = (): JSX.Element => {
                             <input
                                 type="submit"
                                 className="mt-5 w-full bg-blue-800 hover:bg-blue-600 hover:cursor-pointer p-3 text-white uppercase font-bold text-lg"
-                                value="Add Client" />
+                                value={editing ? 'Update Client' : 'Add Client'} />
                         </FormikForm>
 
                 )}}
