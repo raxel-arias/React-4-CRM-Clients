ef546941265014839f102598204dac068db0f33d
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-31 17:25:49 -0500

    Add interface to Form.tsx Component

--- src/interfaces/form/form-component-props.interface.ts
+++ src/interfaces/form/form-component-props.interface.ts
@@ -1,3 +1,10 @@
+import {ClientGot} from "../../services/dto/get-client.dto";
+
+export interface FormCP {
+    editing: boolean,
+    clientToEdit?: ClientGot
+}
+
 export interface ValidationErrorCP {
     msg: string
 }
