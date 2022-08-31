2ff54a9ccc137286f0e6731fccca48a606f84c50
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-31 17:27:33 -0500

    Refactor from simple ClientGot object to clientsDataState

--- src/interfaces/home/clients-table/client-component-props.interface.ts
+++ src/interfaces/home/clients-table/client-component-props.interface.ts
@@ -1,5 +1,7 @@
 import {ClientGot} from "../../../services/dto/get-client.dto";
+import {ClientsDataState} from "../../state/states.interface";
 
 export interface ClientsCP {
-    client: ClientGot
+    client: ClientGot,
+    clientsDataState: ClientsDataState
 }
